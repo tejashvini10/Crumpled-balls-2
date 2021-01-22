0d9a401726faf25dd1c2309eea29da624475de95
tejashvini10 <74137391+tejashvini10@users.noreply.github.com>
2021-01-22 16:35:05 +0530

    Update dustbin.js

--- dustbin.js
+++ dustbin.js
@@ -1,32 +1,59 @@
 class Dustbin {
-    constructor(x,y,width,height){
-        var options={
-    isStatic:true
-    
-        }
-        this.body=Bodies.rectangle(x,y,width,height,options);
-        this.width=width;
-        this.height=height;
-      
+    constructor(x,y){
        
-
-    
-     World.add(world,this.body);
-    
+        this.x=x;
+		this.y=y;
+          this.width=200;
+        this.height=220;
+        this.wallThickness=20;
+        this.image=loadImage("dustbin.png")
+        this.bottomBody=Bodies.rectangle(this.x, this.y, this.width, this.wallThickness, {isStatic:true})
+		this.leftWallBody=Bodies.rectangle(this.x-this.width/2, this.y-this.height/2, this.wallThickness, this.height, {isStatic:true})
+		
+		this.rightWallBody=Bodies.rectangle(this.x+this.width/2, this.y-this.height/2, this.wallThickness, this.height, {isStatic:true})
+		
+		World.add(world, this.bottomBody)
+		World.add(world, this.leftWallBody)
+		World.add(world, this.rightWallBody);
+      
+ 
    
-    };
+    }
     display()
     {
-    var dustbinpos=this.body.position;
-    var angle=this.body.angle;
-    push()
-    translate(dustbinpos.x,dustbinpos.y);
-    rotate(angle);
-    rectMode(CENTER);
+        var posBottom=this.bottomBody.position;
+			var posLeft=this.leftWallBody.position;
+			var posRight=this.rightWallBody.position;
+        
+  
+           push()
+           translate(posLeft.x, posLeft.y);
+           angleMode(RADIANS)
+           fill(255)
+           rotate(this.angle)
+           rectMode(CENTER);
+  
+            pop()
     
-    fill(255,0,0);
-    rect(0,0,this.width,this.height);
-    pop()
-    }
-    }
+    push()
+			translate(posRight.x, posRight.y);
+			rectMode(CENTER)
+			
+			angleMode(RADIANS)
+			fill(255)
+			rotate(-1*this.angle)
+			
+			pop()
+    
+    push()
+			translate(posBottom.x, posBottom.y+10);
+			rectMode(CENTER)
+			
+			angleMode(RADIANS)
+			fill(255)
+			imageMode(CENTER);
+			image(this.image, 0,-this.height/2,this.width, this.height)
+			pop()
+}
+}
     
